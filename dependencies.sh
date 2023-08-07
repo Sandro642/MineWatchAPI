910687150be0e80f7eda6ddceb5144f86a5c5cf7
Sandro642 <sandro33810@gmail.com>
2023-08-08 01:08:31 +0200

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -4,19 +4,19 @@
 function install_if_not_exists() {
     if ! npm list "$1" > /dev/null 2>&1; then
         echo "Installation de $1..."
-        npm install "$1"
+        npm "$1"
     else
         echo "$1 est déjà installé."
     fi
 }
 
 # Installer les dépendances souhaitées
-install_if_not_exists "express"
-install_if_not_exists "mysql2"
-install_if_not_exists "npm install colors"
+install_if_not_exists "install express"
+install_if_not_exists "install mysql2"
+install_if_not_exists "install colors"
 
 #Last command
-install_if_not_exists "npm install"
+install_if_not_exists "install"
 # Ajoutez d'autres dépendances souhaitées ici
 
 echo "Toutes les installations npm sont terminées."
