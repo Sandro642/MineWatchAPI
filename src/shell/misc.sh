76f59fa75661a44bb80e3dc3f3407fcb9891799b
Sandro642 <sandro33810@gmail.com>
2023-08-08 22:39:33 +0200

    remove api.js

--- src/shell/misc.sh
+++ src/shell/misc.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-echo: pm2 start ./src/api/api.js
+echo "Lancement de pm2 pour api.js"
+pm2 start ./src/api/api.js
 
-echo: chmod u+x ./src/shell/dependencies.sh
+echo "Modification des permissions pour dependencies.sh"
+chmod u+x ./src/shell/dependencies.sh
 
-echo: npm run minewatch:dependencies
+echo "Exécution de minewatch:dependencies"
+npm run minewatch:dependencies
 
-echo: npm run minewatch:launch
+echo "Exécution de minewatch:launch"
+npm run minewatch:launch
