21cc4b6e531a345e9b25939bddb3199fb79cead3
Sandro642 <sandro33810@gmail.com>
2023-08-09 14:50:22 +0200

    Add version dev

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,11 @@
 const http = require('http');
-const url = require('url');
 const mysql = require('mysql');
-
-let data = [];
 
 // Autoriser toutes les origines (à utiliser uniquement pour le développement)
 const headers = {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
-    'Access-Control-Allow-Headers': 'Authorization, Content-Type'
+    'Access-Control-Allow-Headers': 'Authorization, Content-Type',
 };
 
 // Configuration de la base de données
@@ -16,48 +13,47 @@ const dbConfig = {
     host: 'mysql-hungrycodes.alwaysdata.net',
     user: '307217_sandro',
     password: 'Sandro3345',
-    database: 'hungrycodes_minewatch'
+    database: 'hungrycodes_minewatch',
 };
 
 // Créer une connexion à la base de données
-const connection = mysql.createConnection(dbConfig);
+const conn = mysql.createConnection(dbConfig);
 
-// Établir la connexion à la base de données
-connection.connect(function(err) {
+// Se connecter à la base de données
+conn.connect((err) => {
     if (err) {
-        console.log('Erreur de connexion à la base de données :', err);
+        console.error('Erreur de connexion à la base de données :', err);
         return;
     }
+
     console.log('Connecté à la base de données.');
 });
 
-// Créer un serveur HTTP
-const server = http.createServer(function(req, res) {
-    // Activer les en-têtes CORS
+// Créer le serveur HTTP
+const server = http.createServer((req, res) => {
     res.writeHead(200, headers);
 
     // Récupérer les données de la table_joueurs avec le temps de l'action
     const sql = 'SELECT *, DATE_FORMAT(Timestamp, "%Y-%m-%d %H:%i:%s") as Timestamp FROM table_joueurs';
-    connection.query(sql, function(err, result) {
+    conn.query(sql, (err, result) => {
         if (err) {
-            console.log('Erreur lors de la requête SQL :', err);
-            res.end('Une erreur s\'est produite.');
+            console.error('Erreur lors de la requête SQL :', err);
+            res.end(JSON.stringify({ message: 'Erreur lors de la récupération des données.' }));
             return;
         }
 
         if (result.length > 0) {
             // Renvoyer les données au format JSON
-            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(200, { ...headers, 'Content-Type': 'application/json' });
             res.end(JSON.stringify(result));
         } else {
-            res.end('Aucune donnée trouvée.');
+            res.end(JSON.stringify({ message: 'Aucune donnée trouvée.' }));
         }
-
-        data = result;
     });
 });
 
-module.exports = {
-    data,
-    connection
-}
+// Démarrer le serveur sur le port 3000
+const port = 3000;
+server.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+});
